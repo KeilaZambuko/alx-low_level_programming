f0acf491880a87e091e3bf2f0c1e5372561a8972
KeilaZambuko <keilazambuko@gmail.com>
2023-02-28 05:46:37 -0800

    swap int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 
 /**
  * swap_int - swaps the values of two integers.
- * @a: first int 
- * @b: second int 
+ * @a: first int
+ * @b: second int
  * Return: Always 0
  */
 
@@ -14,5 +14,5 @@ void swap_int(int *a, int *b)
 
 	c = *a;
 	*a = *b;
-	*b = c;	
+	*b = c;
 }
