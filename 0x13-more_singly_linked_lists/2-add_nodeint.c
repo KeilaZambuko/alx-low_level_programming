12baa56115b6b311dc9af0a60e642faeafd61aee
KeilaZambuko <keilazambuko@gmail.com>
2023-04-05 13:53:58 -0700

    Adds a new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,14 +12,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
-	if (ptr == NULL)
-		return (NULL);
-
 	if (head == NULL)
 		return (NULL);
 	ptr = malloc(sizeof(listint_t));
-	ptr->next = *head;
+	if (ptr == NULL)
+		return (NULL);
 	ptr->n = n;
+	ptr->next = *head;
 	*head = ptr;
 	return (ptr);
 }
